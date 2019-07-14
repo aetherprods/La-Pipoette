ee5a56c4e5550c3057701b62338f0776be6a556f
Jorge Barreto <aetherialprods@gmail.com>
2019-07-14 05:42:06 -0400

    fix title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Head from 'next/head';
 const Index = () => (<div>
     <Head>
         {/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" /> */}
-        <title>Poopycakes</title>
+        <title>La Pipoette</title>
     </Head>
     <div>
         <Lobby />
